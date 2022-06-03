8223daaf631fabaee17a3d3ce20878faf4c401c9
Mike Peterson <mikemikemike3390@yahoo.com>
2022-06-02 18:22:20 -0700

    finished

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,12 @@
 const mysql = require('mysql2');
 
-// Connect to database
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'Moptop00!',
-      database: 'election'
-    },
-    console.log('Connected to the election database.')
-  );
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: '',
+  database: 'election'
+});
 
-
-
-
-
-
-
-
-
-  module.exports = db;
+module.exports = db;
